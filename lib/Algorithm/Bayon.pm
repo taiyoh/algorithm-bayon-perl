cef3c7a94f3179cc1a311a83139d9a37ff0a85f1
taiyoh tanaka <sun.basix@gmail.com>
2009-07-16 22:26:23 +0900

    initial commit

---
+++ lib/Algorithm/Bayon.pm
@@ -0,0 +1,33 @@
+package Algorithm::Bayon;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Algorithm::Bayon -
+
+=head1 SYNOPSIS
+
+  use Algorithm::Bayon;
+
+=head1 DESCRIPTION
+
+Algorithm::Bayon is
+
+=head1 AUTHOR
+
+taiyoh E<lt>sun.basix@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
